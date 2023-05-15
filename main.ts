53c3563f3a6d5bb9d26d4dc94b963f944659a95a
adeline721933 <122544047+adeline721933@users.noreply.github.com>
2023-05-15 12:16:57 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,128 +1,39 @@
-radio.onReceivedValue(function (name, value) {
-    comment.comment("DO NOT CHANGE!")
-    if (name == "x") {
-        xValue = value
-    }
-    if (name == "y") {
-        yValue = value
-    }
-    if (name == "A") {
-        action = "A"
-    }
-    if (name == "B") {
-        action = "B"
-    }
-    if (name == "AB") {
-        action = "AB"
-    }
-    if (name == "C") {
-        action = "C"
-    }
-    if (name == "D") {
-        action = "D"
-    }
-    if (name == "E") {
-        action = "E"
-    }
-    if (name == "F") {
-        action = "F"
-    }
+radio.onReceivedNumber(function (receivedNumber) {
+    I = receivedNumber
 })
-let SecondSonar = 0
-let action = ""
-let yValue = 0
-let xValue = 0
+let V = 0
+let I = 0
 comment.comment("CHANGE THE RADIO GROUP TO YOUR TEAM'S JOYSTICK NUMBER")
 radio.setGroup(29)
+I = 99
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 basic.showIcon(IconNames.Yes)
 basic.showString("R")
-let Sonar = 0
 basic.forever(function () {
-    if (action == "A") {
-        comment.comment("Insert A-Button Action Between These Comments")
-        Sonar = 0
-        comment.comment("Insert A-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "B") {
-        comment.comment("Insert B-Button Action Between These Comments")
-        Sonar = 1
-        comment.comment("Insert B-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "AB") {
-        comment.comment("Insert A/B-Button Action Between These Comments")
-        comment.comment("Insert A/B-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "C") {
-        comment.comment("Insert C-Button Action Between These Comments")
-        comment.comment("Insert C-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "D") {
-        comment.comment("Insert D-Button Action Between These Comments")
-        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 200)
-        comment.comment("Insert D-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "E") {
-        comment.comment("Insert E-Button Action Between These Comments")
-        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 310)
-        comment.comment("Insert E-Button Action Between These Comments")
-        action = "Z"
-    } else if (action == "F") {
-        comment.comment("Insert F-Button Action Between These Comments")
-        comment.comment("Insert F-Button Action Between These Comments")
-        action = "Z"
-    } else {
-        comment.comment("DO NOT CHANGE!")
-        wuKong.setMotorSpeed(wuKong.MotorList.M1, yValue + xValue)
-        wuKong.setMotorSpeed(wuKong.MotorList.M2, yValue - xValue)
+    if (I == 1) {
+        if (V < 90) {
+            V = V + 2
+            basic.pause(100)
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
+        }
+    } else if (I == 2) {
+        V = V - 2
+        basic.pause(100)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
     }
-})
-basic.forever(function () {
-    if (Sonar == 1) {
-        wuKong.setAllMotor(100, 100)
-        SecondSonar = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
-        if (SecondSonar < 60 && SecondSonar > 3) {
-            wuKong.setAllMotor(50, -100)
-            basic.pause(randint(0, 10))
-        } else {
-            wuKong.setAllMotor(100, 100)
-        }
-    } else {
-        if (action == "A") {
-            comment.comment("Insert A-Button Action Between These Comments")
-            Sonar = 0
-            comment.comment("Insert A-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "B") {
-            comment.comment("Insert B-Button Action Between These Comments")
-            Sonar = 1
-            comment.comment("Insert B-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "AB") {
-            comment.comment("Insert A/B-Button Action Between These Comments")
-            comment.comment("Insert A/B-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "C") {
-            comment.comment("Insert C-Button Action Between These Comments")
-            comment.comment("Insert C-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "D") {
-            comment.comment("Insert D-Button Action Between These Comments")
-            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 200)
-            comment.comment("Insert D-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "E") {
-            comment.comment("Insert E-Button Action Between These Comments")
-            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 310)
-            comment.comment("Insert E-Button Action Between These Comments")
-            action = "Z"
-        } else if (action == "F") {
-            comment.comment("Insert F-Button Action Between These Comments")
-            comment.comment("Insert F-Button Action Between These Comments")
-            action = "Z"
-        } else {
-            comment.comment("DO NOT CHANGE!")
-            wuKong.setMotorSpeed(wuKong.MotorList.M1, yValue + xValue)
-            wuKong.setMotorSpeed(wuKong.MotorList.M2, yValue - xValue)
-        }
+    if (I == 3) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 25)
+    }
+    if (I == 4) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, -35)
+    }
+    if (I == 5) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, -80)
+    }
+    if (I == 6) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 80)
+    }
+    if (I == 0) {
+        wuKong.stopAllMotor()
     }
 })
